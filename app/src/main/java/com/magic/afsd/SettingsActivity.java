982739301142f5eab7bd0966435cda517a5512ce
homelessboy <416422546@qq.com>
2016-06-21 15:12:22 +0800

    暂存

--- app/src/main/java/com/magic/afsd/SettingsActivity.java
+++ app/src/main/java/com/magic/afsd/SettingsActivity.java
@@ -1,8 +1,16 @@
 package com.magic.afsd;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import com.magic.afsd.ui.ColorPreference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -16,10 +24,55 @@ import android.preference.PreferenceActivity;
  * API Guide</a> for more information on developing a Settings UI.
  */
 public class SettingsActivity extends PreferenceActivity {
-
+    List<Preference> preferenceList=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+        preferenceList.add(findPreference("connection"));
+        preferenceList.add(findPreference("circle_num"));
+        preferenceList.add(findPreference("surface_num"));
+        preferenceList.add(findPreference("operate_single"));
+        preferenceList.add(findPreference("operate_double_same"));
+        preferenceList.add(findPreference("operate_double_dis"));
+        preferenceList.add(findPreference("rotation_time"));
+        preferenceList.add(findPreference("mask_round"));
+        preferenceList.add(findPreference("color_0"));
+        preferenceList.add(findPreference("color_1"));
+        preferenceList.add(findPreference("color_2"));
+        preferenceList.add(findPreference("color_3"));
+        preferenceList.add(findPreference("color_4"));
+        preferenceList.add(findPreference("color_5"));
+        preferenceList.add(findPreference("standby_index"));
+        preferenceList.add(findPreference("standby_time"));
+        for (Preference preference : preferenceList) {
+            bindPreferenceChange(preference);
+        }
     }
+
+    private static void bindPreferenceChange(Preference preference) {
+        preference.setOnPreferenceChangeListener(preferenceChangeListener);
+        System.out.println(preference.getKey());
+        if (preference instanceof ListPreference)
+            preferenceChangeListener.onPreferenceChange(preference,
+                    PreferenceManager
+                            .getDefaultSharedPreferences(preference.getContext())
+                            .getString(preference.getKey(), ""));
+    }
+
+    private static Preference.OnPreferenceChangeListener preferenceChangeListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+                preference.setSummary(
+                        index >= 0
+                                ? listPreference.getEntries()[index]
+                                : null);
+            }
+            return true;
+        }
+    };
 }
---
+++ app/src/main/java/com/magic/afsd/ui/ColorPreference.java
@@ -0,0 +1,115 @@
+package com.magic.afsd.ui;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.magic.afsd.R;
+
+
+/**
+ * @author: afsd
+ * @version: ${VERSION}
+ */
+public class ColorPreference extends DialogPreference {
+    private Context mContext;
+    private Drawable icon;
+    private int iconId;
+    private int mColor = 0xff000000;
+    private int r = 0;
+    private int g = 0;
+    private int b = 0;
+    private int inputColor=-1;
+    EditText editTextR;
+    EditText editTextG;
+    EditText editTextB;
+
+    public ColorPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        this.mContext = context;
+        iconId = attrs.getAttributeResourceValue(0, -1);
+        if (iconId > 0) {
+            icon = context.getDrawable(iconId);
+            setIcon(icon);
+            if (attrs.getAttributeName(1).equals("color")) {
+                setmColor(attrs.getAttributeIntValue(1, 0));
+            }
+        }
+    }
+
+    public void setmColor(int value) {
+        if (mColor != value) {
+            mColor = value;
+//            r = mColor >> 16 & 0xFF;
+//            g = mColor >> 8 & 0xFF;
+//            b = mColor & 0xFF;
+            persistInt(value);
+            icon.setColorFilter(mColor, PorterDuff.Mode.SRC_OVER);
+            notifyChanged();
+        }
+    }
+
+    public ColorPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public ColorPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.dialogPreferenceStyle);
+    }
+
+    public ColorPreference(Context context) {
+        this(context, null);
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
+        super.onPrepareDialogBuilder(builder);
+
+        final Context context = builder.getContext();
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.color_preference_layout, null);
+        editTextR = (EditText) view.findViewById(R.id.color_r);
+        editTextG = (EditText) view.findViewById(R.id.color_g);
+        editTextB = (EditText) view.findViewById(R.id.color_b);
+
+        editTextR.setText((mColor >> 16 & 0xFF)+ "");
+        editTextG.setText((mColor >> 8 & 0xFF)+ "");
+        editTextB.setText((mColor & 0xFF)+ "");
+        builder.setView(view);
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if(positiveResult){
+            r = Integer.valueOf(String.valueOf(editTextR.getText()));
+            g = Integer.valueOf(String.valueOf(editTextG.getText()));
+            b = Integer.valueOf(String.valueOf(editTextB.getText()));
+            if (r > 255 || g > 255 || b > 255) {
+                Toast toast = new Toast(mContext);
+                toast.setText("输入错误");
+                toast.show();
+                return;
+            }
+            int value=0xFF000000;
+            value=r<<16|value;
+            value=g<<8|value;
+            value=b|value;
+            if(callChangeListener(mColor))
+                setmColor(value);
+        }
+
+    }
+}
